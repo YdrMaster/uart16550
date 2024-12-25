c0526b990a5d45491690bd43fe6886da83e6d0ca
Zhouqi Jiang <luojia@hust.edu.cn>
2024-12-25 08:40:35 +0800

    feat: embedded-hal-nb implementation for Uart16550<R>
    Signed-off-by: Zhouqi Jiang <luojia@hust.edu.cn>

--- src/lib.rs
+++ src/lib.rs
@@ -183,10 +183,8 @@ fn blocking_write<R: Register>(uart: &Uart16550<R>, buf: &[u8]) -> usize {
 }
 
 #[inline]
-fn blocking_flush<R: Register>(uart: &Uart16550<R>) {
-    while !uart.lsr.read().is_transmitter_empty() {
-        core::hint::spin_loop();
-    }
+fn is_transfer_complete<R: Register>(uart: &Uart16550<R>) -> bool {
+    uart.lsr.read().is_transmitter_empty()
 }
 
 impl<R: Register> embedded_io::ErrorType for Uart16550<R> {
@@ -208,6 +206,44 @@ impl<R: Register> embedded_io::Write for Uart16550<R> {
 
     #[inline]
     fn flush(&mut self) -> Result<(), Self::Error> {
-        Ok(blocking_flush(self))
+        while !is_transfer_complete(self) {
+            core::hint::spin_loop();
+        }
+        Ok(())
+    }
+}
+
+impl<R: Register> embedded_hal_nb::serial::ErrorType for Uart16550<R> {
+    type Error = core::convert::Infallible;
+}
+
+impl<R: Register> embedded_hal_nb::serial::Read for Uart16550<R> {
+    #[inline]
+    fn read(&mut self) -> nb::Result<u8, Self::Error> {
+        let mut buf = [0];
+        let len = blocking_read(self, &mut buf);
+        match len {
+            0 => Err(nb::Error::WouldBlock),
+            _ => Ok(buf[0])
+        }
+    }
+}
+
+impl<R: Register> embedded_hal_nb::serial::Write for Uart16550<R> {
+    #[inline]
+    fn write(&mut self, word: u8) -> nb::Result<(), Self::Error> {
+        let len = blocking_write(self, &[word]);
+        match len {
+            0 => Err(nb::Error::WouldBlock),
+            _ => Ok(())
+        }
+    }
+
+    #[inline]
+    fn flush(&mut self) -> nb::Result<(), Self::Error> {
+        match is_transfer_complete(self) {
+            true => Ok(()),
+            false => Err(nb::Error::WouldBlock),
+        }
     }
 }
